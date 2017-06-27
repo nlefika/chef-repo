5b17d0e372ee6ac081898deec1f2b3af25b36a61
Lefika Ngwenya <lefika.ngwenya@gmail.com>
2017-06-27 05:21:53 -0400

    #7 Add home page

--- webserver.rb
+++ webserver.rb
@@ -3,3 +3,12 @@ package 'httpd'
 service 'httpd' do
   action [:enable, :start]
 end
+
+
+file '/var/www/html/index.html' do
+  content '<html>
+    <body>
+      <h1>Hello World</h1>
+    </body>
+  </html>'
+end
