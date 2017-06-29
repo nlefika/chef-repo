0dc152d7ad56ae4faec4447350e411a1cbcd7fcc
Lefika <lefika.ngwenya@gmail.com>
2017-06-29 11:20:32 +0200

    #15 Resolve errors: Add group and user before providing ownership.

--- cookbooks/learn_chef_httpd/recipes/default.rb
+++ cookbooks/learn_chef_httpd/recipes/default.rb
@@ -9,6 +9,14 @@ service 'httpd' do
   action [:enable, :start]
 end
 
+group 'web_admin'
+
+user 'web_admin' do
+  group 'web_admin'
+  system true
+  shell '/bin/bash'
+end
+
 template '/var/www/html/index.html' do
   source 'index.html.erb'
   mode '0644'
