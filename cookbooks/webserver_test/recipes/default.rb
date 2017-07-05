b97a20cf170ea202fc73612a91f2b701bc93f374
Lefika <lefika.ngwenya@gmail.com>
2017-07-05 12:06:55 +0200

    #21 Fix the tests: Serve a page on port 80

--- cookbooks/webserver_test/recipes/default.rb
+++ cookbooks/webserver_test/recipes/default.rb
@@ -7,6 +7,15 @@
 # Install the Apache package.
 package 'httpd'
 
+# Enable and start the service
 service 'httpd' do
   action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>Hello, world</h1>
+  </body>
+</html>'
 end
