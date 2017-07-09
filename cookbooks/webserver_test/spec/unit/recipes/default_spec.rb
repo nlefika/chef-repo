06fad09378dedca3a23aa1b016cab37746877a3d
lefika <ngwenya.ron@gmail.com>
2017-07-09 17:36:34 +0200

    #23 Refactor the tests to reduce repeated code

--- cookbooks/webserver_test/spec/unit/recipes/default_spec.rb
+++ cookbooks/webserver_test/spec/unit/recipes/default_spec.rb
@@ -6,12 +6,12 @@
 
 require 'spec_helper'
 
-describe 'webserver_test::default' do
-  context 'When run on CentOS 7.3.1611' do
+shared_examples 'webserver_test' do |platform, version, package, service|
+  context "When run on #{platform} #{version}" do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
+      runner = ChefSpec::ServerRunner.new(platform: platform, version: version)
       runner.converge(described_recipe)
     end
 
@@ -19,41 +19,26 @@ describe 'webserver_test::default' do
       expect { chef_run }.to_not raise_error
     end
 
-    it 'installs httpd' do
-      expect(chef_run).to install_package 'httpd'
+    it "installs httpd #{package}" do
+      expect(chef_run).to install_package package
     end
 
-    it 'enables the httpd service' do
-      expect(chef_run).to enable_service 'httpd'
+    it "enables the #{service} service" do
+      expect(chef_run).to enable_service service
     end
 
-    it 'starts the httpd service' do
-      expect(chef_run).to start_service 'httpd'
+    it "starts the #{service} service" do
+      expect(chef_run).to start_service service
     end
   end
+end
 
-  context 'when run on Ubuntu 16.04' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-
-    it 'installs apache2' do
-      expect(chef_run).to install_package 'apache2'
-    end
-
-    it 'enables the apache2 service' do
-      expect(chef_run).to enable_service 'apache2'
-    end
-
-    it 'starts the apache2 service' do
-      expect(chef_run).to start_service 'apache2'
-    end
+describe 'webserver_test::default' do
+  platforms = {
+      'centos' => ['7.3.1611', 'httpd', 'httpd'],
+      'ubuntu' => ['16.04', 'apache2', 'apache2']
+  }
+  platforms.each do |platform, platform_data|
+    include_examples 'webserver_test', platform, *platform_data
   end
 end
