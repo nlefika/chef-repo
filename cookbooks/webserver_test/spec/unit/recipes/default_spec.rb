4f3868001b680d819deda31272d93f6585cd09f2
lefika <ngwenya.ron@gmail.com>
2017-07-09 16:32:51 +0200

    #22 Write centos tests

--- cookbooks/webserver_test/spec/unit/recipes/default_spec.rb
+++ cookbooks/webserver_test/spec/unit/recipes/default_spec.rb
@@ -7,16 +7,32 @@
 require 'spec_helper'
 
 describe 'webserver_test::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'When run on CentOS 7.3.1611' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.3.1611')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    it 'installs httpd' do
+      expect(chef_run).to install_package 'httpd'
+    end
+
+    it 'enables the httpd service' do
+      expect(chef_run).to enable_service 'httpd'
+    end
+
+    it 'starts the httpd service' do
+      expect(chef_run).to start_service 'httpd'
+    end
+  end
+
+  context 'when run on Ubuntu 14.04' do
+    #
   end
 end
