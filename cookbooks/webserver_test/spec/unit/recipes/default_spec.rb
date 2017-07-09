9019798bbea48774350b33fe5547d9f3d6f6ebb0
lefika <ngwenya.ron@gmail.com>
2017-07-09 17:04:26 +0200

    #23 Write ubuntu tests

--- cookbooks/webserver_test/spec/unit/recipes/default_spec.rb
+++ cookbooks/webserver_test/spec/unit/recipes/default_spec.rb
@@ -32,7 +32,28 @@ describe 'webserver_test::default' do
     end
   end
 
-  context 'when run on Ubuntu 14.04' do
-    #
+  context 'when run on Ubuntu 16.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+    it 'installs apache2' do
+      expect(chef_run).to install_package 'apache2'
+    end
+
+    it 'enables the apache2 service' do
+      expect(chef_run).to enable_service 'apache2'
+    end
+
+    it 'starts the apache2 service' do
+      expect(chef_run).to start_service 'apache2'
+    end
   end
 end
